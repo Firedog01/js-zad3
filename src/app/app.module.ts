5389449bba148edeb5c27b4cbf3d4205baf31d37
Firedog01 <jan.rubacha@onet.pl>
2022-11-02 11:37:31 +0100

    created desired components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MoviesTableComponent } from './movies-table/movies-table.component';
+import { MoviesByGenreComponent } from './movies-by-genre/movies-by-genre.component';
+import { MoviesByCastComponent } from './movies-by-cast/movies-by-cast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    MoviesTableComponent,
+    MoviesByGenreComponent,
+    MoviesByCastComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movies-by-cast/movies-by-cast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-by-cast',
+  templateUrl: './movies-by-cast.component.html',
+  styleUrls: ['./movies-by-cast.component.css']
+})
+export class MoviesByCastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
