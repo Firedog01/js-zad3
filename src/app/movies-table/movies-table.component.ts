cc8e6c90a1c2ff4a6da253d8379f0a33b39663d6
Firedog01 <jan.rubacha@onet.pl>
2022-11-27 18:43:02 +0100

    load movies from json

--- src/app/movies-table/movies-table.component.ts
+++ src/app/movies-table/movies-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../movie';
 import { SearchObj } from '../search-obj';
+import movies from '../../assets/movies.json';
 
 @Component({
   selector: 'app-movies-table',
@@ -11,7 +12,9 @@ export class MoviesTableComponent implements OnInit {
 
   @Input() search?: SearchObj;
   
-  movies: Movie[] = [];
+  allMovies: any = movies;
+  
+  displayedMovies: Movie[] = [];
   
   constructor() { }
 
@@ -20,5 +23,8 @@ export class MoviesTableComponent implements OnInit {
   }
   
   ngOnInit(): void {
+    for (let i = 0; i < 10; i++) {
+      this.displayedMovies.push(this.allMovies[i]);
+    }
   }
 }
